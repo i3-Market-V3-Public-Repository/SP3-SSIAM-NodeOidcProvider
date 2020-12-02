a1888ab169fa3af8d4c27c84847f5dd24579b3ce
Fernando Román García <fernando.roman@upc.edu>
2020-12-02 16:42:40 +0100

    Development node_modules are now a volume

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,5 +3,10 @@ if [ ! -f "$HOME/.ssh/id_rsa" ]; then
     ssh-keygen -b 2048 -t rsa -f $HOME/.ssh/id_rsa -q -N ""
 fi
 
+if [ -z "$(ls -A ./node_modules)"  ]; then
+    echo "Installing dependencies"
+    npm i
+fi
+
 npm start &
 ssh -R 80:localhost:3000 ssh.localhost.run
