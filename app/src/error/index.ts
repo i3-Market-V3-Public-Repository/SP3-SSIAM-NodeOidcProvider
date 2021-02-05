f2134adf071e10ed4b7cc8ddad09a903dd0f8083
Fernando Román García <fernando.roman@upc.edu>
2021-02-05 12:00:46 +0100

    Added rely party registration endpoint Improved OAS description and management

--- app/src/error/index.ts
+++ app/src/error/index.ts
@@ -1,4 +1,5 @@
 // tslint:disable: max-classes-per-file
+import { Request, Response } from 'express'
 
 export class AccountNotFount extends Error {
   constructor (public login: string) {
@@ -9,5 +10,18 @@ export class AccountNotFount extends Error {
 export class IncorrectPassword extends Error {
   constructor (public login: string) {
     super(`Incorrect password for '${login}'`)
+  }
+}
+
+export class ErrorResponse extends Error {
+  constructor (public statusCode: number, public code: number, message: string) {
+    super(message)
+  }
+
+  send (req: Request, res: Response): void {
+    res.status(this.statusCode).send({
+      code: this.code,
+      message: this.message
+    })
   }
 }
