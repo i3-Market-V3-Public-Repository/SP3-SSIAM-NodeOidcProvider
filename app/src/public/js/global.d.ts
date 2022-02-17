3aa417eac09ec9742203fe99080e0604d58076f8
gobberr <rupert.gobber@yahoo.it>
2022-02-17 12:28:04 +0100

    integrated wallet protocol and some vulnerability fix

---
+++ app/src/public/js/global.d.ts
@@ -0,0 +1,5 @@
+
+declare const walletProtocol: typeof import('@i3m/wallet-protocol')
+declare const walletProtocolUtils: typeof import('@i3m/wallet-protocol-utils/src')
+declare const walletProtocolApi: typeof import('@i3m/wallet-protocol-api')
+declare const rawSdr: string
